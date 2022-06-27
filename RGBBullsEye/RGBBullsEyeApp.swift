3fd11d96090295775e924973c578d93d847c8edb
hajinopro <hajinopro@gmail.com>
2022-06-27 21:13:46 +0900

    Initial Commit

---
+++ RGBBullsEye/RGBBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBBullsEyeApp.swift
+//  RGBBullsEye
+//
+//  Created by 하진호 on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct RGBBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
